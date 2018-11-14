fa2774e3f35b82338f55ce442dedb281660e33bc
ThisUsernameStillDoesntExist <mrtrololoisback@yahoo.fr>
2018-11-15 00:32:37 +0100

    Update TestClass.java

--- Drones-Swarm-IA54/src/dronesSwarmSimulation/TestClass.java
+++ Drones-Swarm-IA54/src/dronesSwarmSimulation/TestClass.java
@@ -5,8 +5,9 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("test.");
 		//test
+		System.out.print("test.");
+		
 	}
 
 }
