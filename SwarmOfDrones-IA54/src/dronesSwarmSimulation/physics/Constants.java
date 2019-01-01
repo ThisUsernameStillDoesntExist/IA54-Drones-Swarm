b1cd7b24b7f9c40425a25f7b7586a2b4fa84f86c
Unknown <mrtrololoisback@yahoo.fr>
2019-01-01 17:10:37 +0100

    Added Packages

---
+++ SwarmOfDrones-IA54/src/dronesSwarmSimulation/physics/Constants.java
@@ -0,0 +1,14 @@
+
+package dronesSwarmSimulation.physics;
+
+
+
+public final class Constants {
+	
+	public static final double DegToRad=Math.PI/180;
+	public static final double WsToWh=1.0/3600; //watt per second to watt per hour
+	
+	public static final double MeterToPixel=50;//1 meter in physicsEngine = ? pixels on screen, with a zoom of 1.0    
+    
+    
+}
